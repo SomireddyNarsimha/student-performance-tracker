a8f8fdb4c3d4a96802f9176771a229b8b1ba2391
NarsimhaReddy <somireddynarsimhasomireddy@gmail.com>
2025-07-15 22:52:59 +0530

    Update AttendanceController.java

--- Backend/src/main/java/com/s2p/controller/AttendanceController.java
+++ Backend/src/main/java/com/s2p/controller/AttendanceController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api/attendance")
 public class AttendanceController {
