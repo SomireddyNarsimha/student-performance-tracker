7b69257aa1ba848c35a8a57d2dec22b48b0cc021
NarsimhaReddy <somireddynarsimhasomireddy@gmail.com>
2025-07-15 22:53:49 +0530

    Update ReportController.java

--- Backend/src/main/java/com/s2p/controller/ReportController.java
+++ Backend/src/main/java/com/s2p/controller/ReportController.java
@@ -5,7 +5,7 @@ import com.s2p.service.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-//@CrossOrigin("http://localhost:4200")
+
 //@RestController
 //@RequestMapping("/api/report")
 public class ReportController {
